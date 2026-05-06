9997f2f08cc5099a35696096c90acaa578f04ec3
Eamon Suen <opzo.esuen@gmail.com>
2026-05-06 14:11:13 -0400

    Configure Vite base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: '.',
+  base: '/badminton-planner/',
   build: {
     outDir: 'dist',
     rollupOptions: {
